13fa385e2ec4f9a5b6c8a6ae0e3dbcfdd9944f95
bus503 <bus503@daum.net>
2020-01-07 16:26:52 +0900

    이벤트핸들러 추가

--- src/java_swing_study/chap09/ContentPaneEx2.java
+++ src/java_swing_study/chap09/ContentPaneEx2.java
@@ -4,20 +4,67 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class ContentPaneEx2 extends JFrame {
+public class ContentPaneEx2 extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
+	private JButton btnOK;
+	private JButton btnCancel;
+	private JButton btnIgnore;
 
 	public ContentPaneEx2() {
+		initialize(); //왼쪽디자인으로 이름추가한거임
+	}
+	private void initialize() {
+		setTitle("ContentPane과 jFrame");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300); //좌표
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5)); // 상하좌우의 패딩마진값
-		contentPane.setLayout(new BorderLayout(0, 0)); // 수평과 수직의 갭?
+		contentPane.setBackground(Color.CYAN);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane); //프레임안에 넣어라
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		//수평갭이 5 수직갭이 5 기본값이 원래 5임
+		
+		btnOK = new JButton("OK");
+		btnOK.addActionListener(this);
+		contentPane.add(btnOK);
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(this);
+		contentPane.add(btnCancel);
+		
+		btnIgnore = new JButton("Ignore");
+		btnIgnore.addActionListener(this);
+		contentPane.add(btnIgnore);
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnIgnore) {
+			btnIgnoreActionPerformed(e);
+		}
+		if (e.getSource() == btnCancel) {
+			btnCancelActionPerformed(e);
+		}
+		if (e.getSource() == btnOK) {
+			btnOKActionPerformed(e);
+		}
+	}
+	protected void btnOKActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
+	protected void btnCancelActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
+	protected void btnIgnoreActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
 }
