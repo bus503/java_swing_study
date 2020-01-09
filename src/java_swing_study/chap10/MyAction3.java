bb22804a5bffc9b808681d62b08aa54f481e0c1f
bus503 <bus503@daum.net>
2020-01-09 16:21:17 +0900

    어댑터 존재 이유 예제

--- src/java_swing_study/chap10/MyAction3.java
+++ src/java_swing_study/chap10/MyAction3.java
@@ -1,23 +1,24 @@
 package java_swing_study.chap10;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-
+import javax.swing.JPanel;
 
 public class MyAction3 implements ActionListener {
+	private JPanel contentPane;
+
+	public MyAction3(JPanel contentPane) {
+		this.contentPane = contentPane;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JButton btn02 =  (JButton) e.getSource();
-		if(e.getSource()== Color.YELLOW) {
-			btn02.setBackground(Color.RED);
-		}else {
-			btn02.setBackground(Color.YELLOW);
+		if (contentPane.getBackground() == Color.YELLOW) {
+			contentPane.setBackground(Color.RED);
+		} else {
+			contentPane.setBackground(Color.YELLOW);
 		}
 	}
 
