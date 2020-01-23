d997768cf9296c035b73df5587256fea138eeea5
bus503 <bus503@daum.net>
2020-01-23 16:04:19 +0900

    바티스타밤을 만들어봄

--- src/java_swing_study/chap14/OptionPaneEx.java
+++ src/java_swing_study/chap14/OptionPaneEx.java
@@ -1,23 +1,31 @@
 package java_swing_study.chap14;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Font;
-import java.awt.Color;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JTabbedPane;
+import java_swing_study.chap14.exam.ui.panel.DepartmentPanel;
+import java_swing_study.chap14.exam.ui.StudentMainPanel;
+import java_swing_study.chap14.exam.ui.DepartmentMainPanel;
 
 @SuppressWarnings("serial")
 public class OptionPaneEx extends JFrame implements ActionListener {
@@ -32,7 +40,16 @@ public class OptionPaneEx extends JFrame implements ActionListener {
 	private JButton btnPopupConfirm;
 	private JButton btnPopupMsg;
 	private JLabel lblPopup;
-
+	private JButton btnFileOpen;
+	private JButton btnFileSave;
+	private JLabel lblImg;
+	private JFileChooser chooser;
+	private JButton btnColor;
+	private JLabel lblColor;
+	private JTabbedPane tabbedPane;
+	private JLabel lblTab1;
+	private StudentMainPanel pStudent;
+	private DepartmentMainPanel pDepartment;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -49,7 +66,10 @@ public class OptionPaneEx extends JFrame implements ActionListener {
 
 	
 	public OptionPaneEx() {
+		chooser = new JFileChooser(System.getProperty("user.dir"));
+		
 		initialize();
+		
 	}
 	private void initialize() {
 		setTitle("팝업다이얼로그, 파일다이얼로그, 컬러다이얼로그");
@@ -85,13 +105,51 @@ public class OptionPaneEx extends JFrame implements ActionListener {
 		pPopupDlg.add(lblPopup);
 		
 		pFileDlg = new JPanel();
+		pFileDlg.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "\uD30C\uC77C \uC5F4\uAE30/\uC800\uC7A5 \uB2E4\uC774\uC5BC\uB85C\uADF8", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		pTop.add(pFileDlg);
+		pFileDlg.setLayout(new BoxLayout(pFileDlg, BoxLayout.Y_AXIS));
+		
+		btnFileOpen = new JButton("파일 열기");
+		btnFileOpen.addActionListener(this);
+		pFileDlg.add(btnFileOpen);
+		
+		btnFileSave = new JButton("파일 저장");
+		btnFileSave.addActionListener(this);
+		pFileDlg.add(btnFileSave);
+		
+		lblImg = new JLabel("");
+		pFileDlg.add(lblImg);
 		
 		pColorDlg = new JPanel();
+		pColorDlg.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "\uCEEC\uB7EC \uB2E4\uC774\uC5BC\uB85C\uADF8", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		pTop.add(pColorDlg);
+		pColorDlg.setLayout(new BorderLayout(0, 0));
+		
+		btnColor = new JButton("색 선택");
+		btnColor.addActionListener(this);
+		pColorDlg.add(btnColor, BorderLayout.NORTH);
+		
+		lblColor = new JLabel("바티스타밤");
+		lblColor.setForeground(Color.RED);
+		lblColor.setHorizontalAlignment(SwingConstants.CENTER);
+		lblColor.setFont(new Font("휴먼아미체", Font.BOLD, 25));
+		pColorDlg.add(lblColor, BorderLayout.CENTER);
 		
 		pCenter = new JPanel();
 		contentPane.add(pCenter, BorderLayout.CENTER);
+		pCenter.setLayout(new BorderLayout(0, 0));
+		
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		pCenter.add(tabbedPane, BorderLayout.CENTER);
+		
+		lblTab1 = new JLabel(new ImageIcon(System.getProperty("user.dir")+"\\images\\batis.jpg"));
+		tabbedPane.addTab("바티스타", null, lblTab1, null);
+		
+		pStudent = new StudentMainPanel();
+		tabbedPane.addTab("학생 관리", null, pStudent, null);
+		
+		pDepartment = new DepartmentMainPanel();
+		tabbedPane.addTab("부서 관리", null, pDepartment, null);
 		
 		btnPopupInput.addActionListener(this);
 		btnPopupConfirm.addActionListener(this);
@@ -99,6 +157,15 @@ public class OptionPaneEx extends JFrame implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnColor) {
+			btnColorActionPerformed(e);
+		}
+		if (e.getSource() == btnFileSave) {
+			btnFileSaveActionPerformed(e);
+		}
+		if (e.getSource() == btnFileOpen) {
+			btnFileOpenActionPerformed(e);
+		}
 		if (e.getSource() == btnPopupInput) {
 			btnPopupInputActionPerformed(e);
 		}
@@ -151,4 +218,33 @@ public class OptionPaneEx extends JFrame implements ActionListener {
 		lblPopup.setText(res);
 	}
 	
+	protected void btnFileOpenActionPerformed(ActionEvent e) {
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF & PNG", "jpg", "gif", "png");
+		chooser.setFileFilter(filter);
+		int res = chooser.showOpenDialog(this);//null도 가능
+		
+		if(res != JFileChooser.APPROVE_OPTION) {
+			JOptionPane.showMessageDialog(this, "파일을 선택하지 않았습니다.", "경고", JOptionPane.WARNING_MESSAGE);
+			return;
+		}
+		String filePath = chooser.getSelectedFile().getPath();
+		lblImg.setIcon(new ImageIcon(filePath));
+	}
+	protected void btnFileSaveActionPerformed(ActionEvent e) {
+		int res = chooser.showSaveDialog(this);
+		if(res != JFileChooser.APPROVE_OPTION) {
+			JOptionPane.showMessageDialog(this, "파일을 선택하지 않았습니다.", "경고", JOptionPane.WARNING_MESSAGE);
+			return;
+		}
+		String filePath = chooser.getSelectedFile().getPath();
+		System.out.println(filePath);
+	}
+	protected void btnColorActionPerformed(ActionEvent e) {
+		Color selectColor = JColorChooser.showDialog(null, "Color", Color.YELLOW);
+	
+		if(selectColor != null) {
+			lblColor.setForeground(selectColor);
+		}
+		
+	}
 }
